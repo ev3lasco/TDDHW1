a4e08075130324c6e5bff6bd5c050af3736b9972
[Edwin Velasco] <evelasco1423@gmail.com>
2023-04-20 01:15:53 -0400

    Initial commit

---
+++ src/main/java/calc/Calculator.java
@@ -0,0 +1,39 @@
+package calc;
+
+public class Calculator {
+
+    //red
+    //green
+    //refactor
+
+    public int add(int value1 , int value2){
+        return 0;
+    }
+    public int subtract(int value1, int value2){
+        return 0;
+    }
+    public int multiply ( int value1, int value2){
+        return 0;
+    }
+    public int divide( int value1, int value2){
+        return 0;
+    }
+    public int square (int value1){
+        return 0;
+    }
+    public int squareRoot (int value1){
+        return 0;
+    }
+    public int exponent (int value1){
+        return 0;
+    }
+
+    //subtract
+
+    //multiply
+    //divide
+    //square
+    //squareRoot
+    // exponent
+    //
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,84 @@
+import org.junit.Test;
+import calc.Calculator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class CalculatorTest {
+
+    //red, green refactor
+
+    @Test
+    public void addTest() {
+        //Given
+        Calculator calculator = new Calculator();
+        int val1 = 7;
+        int val2 = 10;
+        int expected = val1 + val2;
+        //When
+        int actual = calculator.add(val1, val2);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void subtractTest() {
+
+        Calculator calculator = new Calculator();
+        int val1 = 7;
+        int val2 = 10;
+        int expected = val2 + val1;
+        int actual = calculator.subtract(val1, val2);
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void multiplyTest() {
+        Calculator calculator = new Calculator();
+        int val1 = 15;
+        int val2 = 15;
+        int expected = val1 * val2;
+        int actual = calculator.multiply(val1, val2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divideTest() {
+        Calculator calculator = new Calculator();
+        int val1 = 10;
+        int val2 = 5;
+        int expected = val1 / val2;
+        int actual = calculator.divide(val1, val2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void squareTest() {
+        Calculator calculator = new Calculator();
+        int val1 = 10;
+        int expected = val1 * val1;
+        int actual = calculator.square(val1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void squareRootTest() {
+        Calculator calculator = new Calculator();
+        int val1 = 10;
+        int expected = (int) Math.sqrt(val1);
+        int actual = calculator.squareRoot(val1);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void exponentTest() {
+        Calculator calculator = new Calculator();
+        int val1 = 10;
+        int expected = (int) Math.exp(val1);
+        int actual = calculator.exponent(val1);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
+
+
